903fc8b86c6e3f8c4eb14aee13b034cad964175b
HaZe_Ruby <shivamkhatri820@gmail.com>
2024-01-21 20:58:29 +0500

    connecting to client

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
 	cors: {
-		origin: "http://localhost:5173",
+		origin: "https://legendary-sundae-dc58ea.netlify.app/",
 		credentials: true,
 	},
 });
